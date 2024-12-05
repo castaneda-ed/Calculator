094f30e7114beafecae4671a63c4875cc48b5a0a
Edwin <edwincastaneda0801@gmail.com>
2024-12-05 09:39:18 -0600

    simple calculator functions

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,40 +1,144 @@
+import { useState } from "react";
+
 export default function Calculator() {
+  const [value, setValue] = useState("");
   return (
     <main className="container ">
       <div className="calculator">
         <form>
           <label>
-            <input type="text" className="display" />
+            <input type="text" className="display" value={value} />
           </label>
           <div>
-            <input type="button" value="C" className="deleting" />
-            <input type="button" value="DE" className="deleting" />
-            <input type="button" value="%" className="deleting" />
-            <input type="button" value="/" className="operator" />
+            <input
+              type="button"
+              value="AC"
+              className="deleting"
+              onClick={(e) => setValue("")}
+            />
+            <input
+              type="button"
+              value="DE"
+              className="deleting"
+              onClick={(e) => setValue(value.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="%"
+              className="deleting"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="/"
+              className="operator"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
           </div>
           <div>
-            <input type="button" value="7" />
-            <input type="button" value="8" />
-            <input type="button" value="9" />
-            <input type="button" value="*" className="operator" />
+            <input
+              type="button"
+              value="7"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="8"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="9"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="*"
+              className="operator"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
           </div>
           <div>
-            <input type="button" value="4" />
-            <input type="button" value="5" />
-            <input type="button" value="6" />
-            <input type="button" value="-" className="operator" />
+            <input
+              type="button"
+              value="4"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="5"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="6"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="-"
+              className="operator"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
           </div>
           <div>
-            <input type="button" value="1" />
-            <input type="button" value="2" />
-            <input type="button" value="3" />
-            <input type="button" value="+" className="operator" />
+            <input
+              type="button"
+              value="1"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="2"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="3"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="+"
+              className="operator"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
           </div>
           <div className="last-row">
-            <input type="button" value="00" />
-            <input type="button" value="0" />
-            <input type="button" value="•" />
-            <input type="button" value="=" className="operator" />
+            <input
+              type="button"
+              value="00"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="0"
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="."
+              style={{ fontSize: "19px" }}
+              className="numbers"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="="
+              className="operator"
+              onClick={(e) => setValue(eval(value))}
+            />
           </div>
         </form>
       </div>
